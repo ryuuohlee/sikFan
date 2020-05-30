238add9abe08584048cd6c777241ce45aa683edc
Jeffrey Lee <jglee1@msn.com>
2020-05-30 02:58:15 -0700

    Feature: Completed Part 1 of Sik fan

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -29,8 +29,8 @@ class Business extends React.Component {
           </div>
           <div className="Business-reviews">
             <h3>{business.category}</h3>
-            <h3 className="rating">{business.rating + ' ' + stars}</h3>
-            <p>{business.reviewCount + ' ' + reviews}</p>
+            <h3 className="rating">{business.rating}</h3>
+            <p>{business.reviewCount}</p>
           </div>
         </div>
       </div>
