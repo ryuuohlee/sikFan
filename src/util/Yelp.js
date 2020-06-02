05fc3f9d8fbf019a3098bef2c6086c30ce90c942
Jeffrey Lee <jglee1@msn.com>
2020-06-02 12:28:21 -0700

    Feature: completed Yelp API connection to app

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -7,21 +7,24 @@ const Yelp = {
         Authorization: `Bearer ${apiKey}`
       }
     }).then(response => {
-        response.json()
-      }).then(jsonResponse => {
+        return response.json()
+      }).then((jsonResponse) => {
         if(jsonResponse.businesses) {
-          return jsonResponse.businesses.map(business => {
-            id: business.id,
-            imageSrc: business.imageSrc,
-            name: business.name,
-            address: business.address,
-            city: business.city,
-            state: business.state,
-            zipCode: business.zipCode,
-            category: business.category,
-            rating: business.rating,
-            reviewCount: business.reviewCount
-          })
+          return jsonResponse.businesses.map(((business) => {
+            console.log(business);
+            return {
+              id: business.id,
+              imageSrc: business.image_url,
+              name: business.name,
+              address: business.location.address1,
+              city: business.location.city,
+              state: business.location.state,
+              zipCode: business.location.zip_code,
+              category: business.categories[0].title,
+              rating: business.rating,
+              reviewCount: business.review_count
+            };
+          }));
         }
     });
   }
