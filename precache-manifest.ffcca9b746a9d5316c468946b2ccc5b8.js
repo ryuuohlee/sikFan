2ef76430bb60b18b48eb6daa8d884d63149c3beb
Jeffrey Lee <jglee1@msn.com>
2020-10-29 08:37:37 -0700

    Updates

---
+++ precache-manifest.ffcca9b746a9d5316c468946b2ccc5b8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a5dd52e30a7e1fe4b22c146a8349a45",
+    "url": "/sikFan/index.html"
+  },
+  {
+    "revision": "54df964fe168ae13945d",
+    "url": "/sikFan/static/css/main.2092905b.chunk.css"
+  },
+  {
+    "revision": "90e7ce98b552e5117c58",
+    "url": "/sikFan/static/js/2.37ca2e79.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sikFan/static/js/2.37ca2e79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54df964fe168ae13945d",
+    "url": "/sikFan/static/js/main.bc843f22.chunk.js"
+  },
+  {
+    "revision": "5d1d82eb51d2d1bd8842",
+    "url": "/sikFan/static/js/runtime-main.81a15165.js"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/sikFan/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/sikFan/static/media/background_search_mobile.5d0905e6.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/sikFan/precache-manifest.1b4da4a85d4ade5ffb3960c77851d9fb.js"
+  "/sikFan/precache-manifest.ffcca9b746a9d5316c468946b2ccc5b8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/sikFan/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
